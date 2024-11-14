8e8713eaff22bc2f29d303e2b72339ab8c4bec7b
Giorgi Kotchlamazashvili <1886698+hertzg@users.noreply.github.com>
2024-11-14 15:29:15 +0100

    feat: initial commit

---
+++ oauth/oauth.ts
@@ -0,0 +1,166 @@
+export const DEFAULT_BASE_URL = "https://oauth.magticom.ge/auth/";
+
+export const DEFAULT_USER_AGENT =
+  "MyMagti/11.9.96 (Magticom.MyMagti; build:1; iOS 18.1.0) Alamofire/5.9.1";
+
+export const DEFAULTS = Object.seal({
+  login: {
+    url: "oauth/token",
+    grantType: "mymagti_auth",
+    clientId: "MymagtiApp2FAPre",
+    authorization:
+      "Basic TXltYWd0aUFwcDJGQVByZTpQaXRhbG9AI2RkZWVyYWFzYXNERjIxMyQl",
+  },
+  refresh: {
+    url: "oauth/token",
+    grantType: "refresh_token",
+    clientId: "MymagtiAppNew",
+    authorization: "Basic TXltYWd0aUFwcE5ldzpQaXRhbG9AI2RkZWVyYWFzYXNERjIxMyQl",
+  },
+});
+
+export type FetchOauthOptions = {
+  fetch?: typeof fetch;
+  url?: string;
+  baseUrl?: string;
+  grantType?: string;
+  clientId?: string;
+  authorization?: string;
+  userAgent?: string;
+};
+
+async function fetchOauth(
+  type: keyof typeof DEFAULTS,
+  subUrl: URL | string,
+  init: RequestInit = {},
+  options: FetchOauthOptions = {}
+) {
+  const {
+    fetch = globalThis.fetch,
+    baseUrl = DEFAULT_BASE_URL,
+    userAgent = DEFAULT_USER_AGENT,
+    authorization = DEFAULTS[type].authorization,
+  } = options;
+
+  const url = new URL(subUrl, baseUrl);
+
+  return await fetch(url.toString(), {
+    ...init,
+    headers: {
+      "User-Agent": userAgent,
+      Authorization: authorization,
+      ...(init.headers || {}),
+    },
+  });
+}
+
+async function oauthGrant(
+  type: keyof typeof DEFAULTS,
+  formdata: Record<string, string | Blob>,
+  options: FetchOauthOptions = {}
+) {
+  const { url, grantType, clientId } = DEFAULTS[type];
+
+  const body = new FormData();
+  body.append("client_id", clientId);
+  body.append("grant_type", grantType);
+  for (const [key, val] of Object.entries(formdata)) {
+    body.append(key, val);
+  }
+
+  return await fetchOauth(
+    type,
+    url,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: body.toString(),
+    },
+    options
+  );
+}
+
+export type FetchTokenRequest =
+  | {
+      type: "login";
+      username: string;
+      password: string;
+    }
+  | {
+      type: "refresh";
+      refreshToken: string;
+    };
+
+export interface GenericErrorResponse {
+  error: string;
+  error_description: string;
+}
+
+export interface TokenInfo {
+  access_token: string;
+  autoLogin: number;
+  expires_in: number;
+  jti: string;
+  phoneNo: string;
+  refresh_token: string;
+  scope: string;
+  token_type: string;
+  userId: number;
+  userIdentifier: string;
+}
+
+export type LoginResult =
+  | {
+      result: "error";
+      statusCode: number;
+      data: GenericErrorResponse;
+    }
+  | {
+      result: "success";
+      statusCode: number;
+      data: TokenInfo;
+    };
+
+export async function fetchToken(
+  request: FetchTokenRequest,
+  options: FetchOauthOptions = {}
+): Promise<LoginResult> {
+  let response;
+  switch (request.type) {
+    case "login":
+      response = await oauthGrant(
+        request.type,
+        {
+          username: request.username,
+          password: request.password,
+        },
+        options
+      );
+      break;
+    case "refresh":
+      response = await oauthGrant(
+        request.type,
+        {
+          refresh_token: request.refreshToken,
+        },
+        options
+      );
+      break;
+  }
+
+  if (response.status !== 200) {
+    return {
+      result: "error",
+      statusCode: response.status,
+      data: (await response.json()) as GenericErrorResponse,
+    };
+  }
+
+  return {
+    result: "success",
+    statusCode: response.status,
+    data: (await response.json()) as TokenInfo,
+  };
+}
---
+++ oauth/oauth_login_test.ts
@@ -0,0 +1,91 @@
+import { afterEach, beforeEach, describe, it } from "@std/testing/bdd";
+import {
+  type Stub,
+  stub,
+  resolvesNext,
+  assertSpyCalls,
+  assertSpyCallAsync,
+} from "@std/testing/mock";
+import { DEFAULT_USER_AGENT, DEFAULTS, fetchToken } from "./oauth.ts";
+import { assertEquals } from "@std/assert/equals";
+
+describe("oauth", () => {
+  let fetchStub: Stub<
+    typeof globalThis,
+    Parameters<typeof globalThis.fetch>,
+    ReturnType<typeof globalThis.fetch>
+  >;
+
+  const FAKE_TOKEN_INFO = {
+    access_token: "fake_access_token",
+    autoLogin: 1,
+    expires_in: 3600,
+    jti: "fake_jti",
+    phoneNo: "fake_phoneNo",
+    refresh_token: "fake_refresh_token",
+    scope: "fake_scope",
+    token_type: "fake_token_type",
+    userId: 1234,
+    userIdentifier: "fake_userIdentifier",
+  };
+
+  beforeEach(() => {
+    fetchStub = stub(
+      globalThis,
+      "fetch",
+      resolvesNext([
+        {
+          status: 200,
+          json: resolvesNext([FAKE_TOKEN_INFO]),
+        } as unknown as Response,
+      ])
+    );
+  });
+
+  afterEach(() => {
+    fetchStub.restore();
+  });
+
+  it("login success", async () => {
+    const FAKE_USERNAME = "test";
+    const FAKE_PASSWORD = "test";
+
+    const result = await fetchToken(
+      {
+        type: "login",
+        username: FAKE_USERNAME,
+        password: FAKE_PASSWORD,
+      },
+      {
+        fetch: fetchStub,
+      }
+    );
+
+    assertSpyCalls(fetchStub, 1);
+
+    const expectedPayload = new FormData();
+    expectedPayload.append("client_id", DEFAULTS.login.clientId);
+    expectedPayload.append("grant_type", DEFAULTS.login.grantType);
+    expectedPayload.append("username", FAKE_USERNAME);
+    expectedPayload.append("password", FAKE_PASSWORD);
+
+    assertSpyCallAsync(fetchStub, 0, {
+      args: [
+        "https://oauth.magticom.ge/auth/oauth/token",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization: DEFAULTS.login.authorization,
+            "User-Agent": DEFAULT_USER_AGENT,
+          },
+          body: expectedPayload.toString(),
+        },
+      ],
+    });
+
+    assertEquals(result.result, "success");
+    assertEquals(result.statusCode, 200);
+    assertEquals(result.data, FAKE_TOKEN_INFO);
+  });
+});
